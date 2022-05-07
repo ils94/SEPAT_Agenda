5ee58e8df3bc7908aa6b3c0cd86cae5d975d5bcb
Igor Lima <igorlimaesilva94@gmail.com>
2022-05-06 22:52:42 -0300

    Pequenos ajustes

--- app/src/main/java/com/droidev/sepatagenda/MainActivity.java
+++ app/src/main/java/com/droidev/sepatagenda/MainActivity.java
@@ -29,6 +29,7 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity implements RecyclerViewClickInterface {
 
     private ArrayList<String> banco;
+    private String data, hora;
 
     RecyclerView RecyclerView;
     RecyclerView.Adapter Adapter;
@@ -38,18 +39,26 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewClick
 
     TinyDB tinyDB;
 
-    public ArrayList<String> dataHora() {
+    public void dataHora() {
 
-        String currentDate = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(new Date());
+        data = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(new Date());
 
-        String currentTime = new SimpleDateFormat("HH:mm", Locale.getDefault()).format(new Date());
+        System.out.println(data);
 
-        ArrayList<String> valores = new ArrayList<>();
+        data = data.replace("/01/", "/jan/")
+                .replace("/02/", "/fev/")
+                .replace("/03/", "/mar/")
+                .replace("/04/", "/abr/")
+                .replace("/05/", "/mai/")
+                .replace("/06/", "/jun/")
+                .replace("/07/", "/jul/")
+                .replace("/08/", "/ago/")
+                .replace("/09/", "/set/")
+                .replace("/10/", "/out/")
+                .replace("/11/", "/nov/")
+                .replace("/12/", "/dez/").toUpperCase();
 
-        valores.add(currentDate);
-        valores.add(currentTime);
-
-        return valores;
+        hora = new SimpleDateFormat("HH:mm", Locale.getDefault()).format(new Date());
     }
 
     public void makeConnection() {
@@ -169,8 +178,7 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewClick
 
             } else {
 
-                String data = (String) dataHora().get(0);
-                String hora = (String) dataHora().get(1);
+                dataHora();
 
                 dbQueries db = new dbQueries();
 
@@ -254,6 +262,10 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewClick
             if (atendente.getText().toString().isEmpty() || dbName.getText().toString().isEmpty() || dbUser.getText().toString().isEmpty() || dbPass.getText().toString().isEmpty() || dbHost.getText().toString().isEmpty() || dbPort.getText().toString().isEmpty()) {
 
                 Toast.makeText(MainActivity.this, "É neccessário preencher todos os campos", Toast.LENGTH_SHORT).show();
+
+            } else if (atendente.getText().toString().length() > 10) {
+
+                Toast.makeText(MainActivity.this, "Nome do Atendente não pode ser maior que 10 caracteres.", Toast.LENGTH_SHORT).show();
             } else {
 
                 tinyDB.remove("atendente");
